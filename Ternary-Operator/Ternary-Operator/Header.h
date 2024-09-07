8b69c6018301363d153f50a18d2be64e8c4e9c7e
MohammedHammouz <81511588+MohammedHammouz@users.noreply.github.com>
2024-09-07 11:30:20 +0300

    Delete Ternary-Operator directory

--- Ternary-Operator/Ternary-Operator/Header.h
+++
@@ -1,9 +0,0 @@
-using namespace std;
-#include <iostream>
-#pragma once
-int ReadNumber() {
-    int number = 0;
-    cout << "Enter any number that you want:\n";
-    cin >> number;
-    return number;
-}
